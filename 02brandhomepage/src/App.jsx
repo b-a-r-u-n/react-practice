1ea507b87b5be88f8dc1603215ef58fff4113628
b-a-r-u-n <barunkumarmahakud40@gmail.com>
2024-06-13 00:56:16 +0530

    Update App.jsx file

--- 02brandhomepage/src/App.jsx
+++ 02brandhomepage/src/App.jsx
@@ -1,12 +1,16 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import NavBar from './COMPONENTS/NavBar';
+import Hero from './COMPONENTS/Hero';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <div className='m-o min-h-screen'
+      style={{background: 'linear-gradient(to right, #dae2f8, #d6a4a4)'}}
+    >
+      <NavBar />
+      <Hero />
+    </div>
   )
 }
 

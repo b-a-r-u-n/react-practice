097f66f757e1e72d9d34c9d290a9f3f482030a77
b-a-r-u-n <barunkumarmahakud40@gmail.com>
2024-06-15 22:45:13 +0530

    Added more data

---
+++ 03shoppingCart/src/COMPONENTS/Data.jsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import img1 from '../IMAGES/img1.jpg';
+import img2 from '../IMAGES/img2.jpg';
+import img3 from '../IMAGES/img3.jpg';
+import img4 from '../IMAGES/img4.png';
+import img5 from '../IMAGES/img5.png';
+import img6 from '../IMAGES/img6.png';
+import img7 from '../IMAGES/img7.png';
+import img8 from '../IMAGES/img8.png';
+
+
+let Data = [
+    {
+        id: 1,
+        name: "Iphone 50 pro MAX | Mobile",
+        image: img1,
+        price: 9999,
+        count: 1
+    },
+    {
+        id: 10,
+        name: "Iphone 50 pro MAX | Mobile",
+        image: img1,
+        price: 9999,
+        count: 1
+    },
+    {
+        id: 11,
+        name: "Iphone 50 pro MAX | Mobile",
+        image: img1,
+        price: 9999,
+        count: 1
+    },
+    {
+        id: 2,
+        name: "Acer predator helios 5000 | Laptop",
+        image: img2,
+        price: 1599,
+        count: 1
+    },
+    {
+        id: 3,
+        name: "PS-500 Gaming Controller",
+        image: img3,
+        price: 499,
+        count: 1
+    },
+    {
+        id: 4,
+        name: "Acer Nitro 12k resolution | Monitor",
+        image: img4,
+        price: 899,
+        count: 1
+    },
+    {
+        id: 5,
+        name: "Mobile Gaming Machine | Fully Loaded",
+        image: img5,
+        price: 399,
+        count: 1
+    },
+    {
+        id: 6,
+        name: "Laptop Computer Desk Table",
+        image: img6,
+        price: 1199,
+        count: 1
+    },
+    {
+        id: 7,
+        name: "8k PC | High Performance | gaming Editing",
+        image: img7,
+        price: 1399,
+        count: 1
+    },
+    {
+        id: 8,
+        name: "Mini Projector",
+        image: img8,
+        price: 199,
+        count: 1
+    },
+]
+
+export default Data;

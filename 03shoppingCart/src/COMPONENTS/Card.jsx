20592e53d05420a5689622ab1db478a87861eb31
b-a-r-u-n <barunkumarmahakud40@gmail.com>
2024-06-15 22:46:43 +0530

    Added some Responsivness

--- 03shoppingCart/src/COMPONENTS/Card.jsx
+++ 03shoppingCart/src/COMPONENTS/Card.jsx
@@ -2,12 +2,12 @@
 const Card = ({product , addToCart}) => {
     let {id , name , image , price} = product;
     return (
-            <div className="background  w-[22vw] h-[50vh]  overflow-hidden">
+            <div className="background  sm:w-[22vw] w-[80%] h-[60vh]  overflow-hidden">
                 <div className="image w-[100%] h-[60%] cursor-pointer"
                     style={{backgroundImage: `url(${image})`}}
                 ></div>
-                <div className="h-[40%] flex flex-col py-[2vh] items-center justify-between">
-                    <h2 className="text-[1.8vw] font-semibold">{name}</h2>
+                <div className=" flex h-[40%] flex-col py-[2vh] items-center justify-between">
+                    <h2 className="text-md font-semibold">{name}</h2>
                     <p className="font-bold text-lg">Price: <span className="text-lg font-bold text-green-600">$ {price}</span></p>
                     <button className="py-[0.5vh] border-2 border-black px-[1vw] text-black font-bold outline-none rounded-md"
                         onClick={() => {addToCart(product)}}
